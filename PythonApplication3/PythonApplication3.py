87cb4417d280c5cdf0ed9c4622b41d2e9ecf47a6
xianshanzhang <xianshan.zhang@outlook.com>
2018-11-08 12:42:44 +0800

    初始化！

--- PythonApplication3/PythonApplication3.py
+++ PythonApplication3/PythonApplication3.py
@@ -1,1 +1,15 @@
+# -*- coding: utf-8 -*-
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import re
+import os
+
+sns.set_style("whitegrid")
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.show()
