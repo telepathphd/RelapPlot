9df266f66ef81f1b433338536b38a007005744e9
xianshanzhang <xianshan.zhang@outlook.com>
2018-11-15 02:12:23 +0800

    暂时取消了超调量在图中的显示；格式切换至新TNG对应的30组数据，取消了输出修正系数x，增加了棒位图，而棒速图暂时有点问题没添加，注释了开环功能，后续通过选择进行补充。

--- PythonApplication3/PythonApplication3.py
+++ PythonApplication3/PythonApplication3.py
@@ -10,18 +10,19 @@ import seaborn as sns
 import numpy as np
 import re
 import os
+from scipy.interpolate import interp1d
+from scipy.optimize import fsolve
 
 sns.set_style("whitegrid")
 plt.rcParams['font.sans-serif'] = ['SimHei'] #用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False #用来正常显示负号
-
 def sub_plt(x,y,labelname,filename,ylabel,xrange):
     plt.show()
     for i in range(0,len(y[1])):
         if i == 0:
-            plt.plot(x,y[:,i],label=labelname[i]+setp_info(y[:,i],x),linewidth=2.0)
+            plt.plot(x,y[:,i],label=labelname[i],linewidth=2.0)
         elif i == 1:
-            plt.plot(x,y[:,i],label=labelname[i]+setp_info(y[:,i],x),linestyle='--',linewidth=2.0)
+            plt.plot(x,y[:,i],label=labelname[i],linestyle='--',linewidth=2.0)# + setp_info(y[:,i],x)
         elif i == 2:
             plt.plot(x,y[:,i],label=labelname[i],linestyle='-.',linewidth=2.0)
         elif i == 3:
@@ -56,7 +57,7 @@ def import_data(filename,flag):
         row = 221
         col = 21
     elif flag == 2:
-        row = 122
+        row = 131
         col = 10
         
     with open(filename,'r') as f:
@@ -167,14 +168,15 @@ def ppp(data,flag,filename):
         name = '反应性'
         ylabel_name = '反应性/$'
         legend_name = ['总合','控制棒','温度']
-        p = np.transpose(np.vstack((data2[:,5],data2[:,6],data2[:,5] - data2[:,6])))
+        p = np.transpose(np.vstack((data[:,5],data[:,6],data[:,5] - data[:,6])))
     elif flag == 14:
         name = '反应性(temp)'
         ylabel_name = '反应性/$'
         legend_name = ['总合','控制棒','燃料','冷却剂']
 #       通过x修正温度反馈，以最终值时燃料、冷却剂反馈+控制棒=总和为准
-        x = (data[-1,5] - data[-1,6]) / ((data[-1,7] - data[0,7]) * -0.005 + (data[-1,8] - data[0,8]) * -0.08)
-        print(x)
+        #x = (data[-1,5] - data[-1,6]) / ((data[-1,7] - data[0,7]) * -0.005 +
+        #(data[-1,8] - data[0,8]) * -0.08)
+        #print(x)
 #        p=np.transpose(np.vstack((data[:,5],data[:,6],(data[:,7]-data[0,7])*-0.005*x,(data[:,8]-data[0,8])*-0.08*x)))
         p = np.transpose(np.vstack((data[:,5],data[:,6],(data[:,7] - data[0,7]) * -0.005,(data[:,8] - data[0,8]) * -0.08)))
     elif flag == 15:
@@ -183,10 +185,22 @@ def ppp(data,flag,filename):
         index = [7]
         legend_name = ['堆芯']
         p = data[:,index] - 273.15
+    elif flag == 16:
+        p=data[:,6]
+        name = '棒位'
+        ylabel_name = 'cm'
+        legend_name = ['控制棒']
+        lookUpTable_rodHeight = np.linspace(10, 130, 13) # cm,10-130
+        breakPoints_Rho = np.array([0, 170, 504,967,1467,1928,2309,2592,2788,2920,3009,3067,3100]) - 2130.39518516 # 反应性
+        rho2height = interp1d(breakPoints_Rho,lookUpTable_rodHeight, kind='cubic')
+        # np.gradient(data) 求导数
+        p = rho2height(data[:,[6]])
+        #p=np.transpose(np.vstack((p,data[:,6])))
+        #p = np.transpose(np.vstack((data[:,5],data[:,6])))
     else:
         print('Unexpected plot type. No plot created.')
     mkdir(path)
-    sub_plt(data[:,0] / 8.6,p,legend_name,filename + '.pdf',ylabel_name,0)
+    sub_plt(data[:,0] / 8.6,p,legend_name,filename + '.png',ylabel_name,0)
     return p
 
 def listdir(path,FP,Condition,num):
@@ -203,42 +217,50 @@ def listdir(path,FP,Condition,num):
 
 path = 'C:\\AppData\\relapdata\\p2'
 mkdir(path + '\\figs')
-for i1 in range(0,4) : # 功率水平（10，20，50，100）4个
-    if i1 == 0:
-        fp = 10
-    elif i1 == 1:
-        fp = 20
-    elif i1 == 2:
-        fp = 50
-    elif i1 == 3:
-        fp = 100
-    for i2 in range(0,3):
-    #工况（反应性0~-0.2，给水流量+10%，蒸汽阀门1~0.9，上充0.9167，下泄-0.9167，喷淋1.167，电热2.7e5）
-        data1=listdir(path,fp,i2+1,1)
-        data2=listdir(path,fp,i2+1,2)
-        if fp == 100:
-            if i2+1 in [1]:
-                data1=data1[range(0,min(int(round((300*8.6/2))),len(data1[:,0]))),:]
-                data2=data2[range(0,min(int(round((300*8.6/2))),len(data1[:,0]))),:]
-            elif i2+1 in [2,3]:
-                data1=data1[range(0,min(int(round((600*8.6/2))),len(data1[:,0]))),:]
-                data2=data2[range(0,min(int(round((600*8.6/2))),len(data1[:,0]))),:]
-        elif fp == 50:
-            if i2+1 in [1]:
-                data1=data1[range(0,min(int(round((300*8.6/2))),len(data1[:,0]))),:]
-                data2=data2[range(0,min(int(round((300*8.6/2))),len(data1[:,0]))),:]
-            elif i2+1 in [2]:
-                data1=data1[range(0,min(int(round((600*8.6/2))),len(data1[:,0]))),:]
-                data2=data2[range(0,min(int(round((600*8.6/2))),len(data1[:,0]))),:]
-        elif fp == 10:
-            if i2+1 in [1]:
-                data1=data1[range(0,min(int(round((300*8.6/2))),len(data1[:,0]))),:]
-                data2=data2[range(0,min(int(round((300*8.6/2))),len(data1[:,0]))),:]
-            elif i2+1 in [3]:
-                data1=data1[range(0,min(int(round((600*8.6/2))),len(data1[:,0]))),:]
-                data2=data2[range(0,min(int(round((600*8.6/2))),len(data1[:,0]))),:]
-        for i3 in range(0,15): #参数响应图14张
-            if i3 < 10:
-                ppp(data1,i3+1,path+'\\figs\\'+str(fp)+'c'+str(i2+1)+'_'+str(i3+1))
-            else:
-                ppp(data2,i3+1,path+'\\figs\\'+str(fp)+'c'+str(i2+1)+'_'+str(i3+1))
+data1 = import_data(path + '\\1.txt',1)
+data2 = import_data(path + '\\2.txt',2)
+for i3 in range(14,16):
+    if i3 < 10:
+        ppp(data1,i3 + 1,path + '\\figs\\' + str(i3 + 1))
+    else:
+        ppp(data2,i3 + 1,path + '\\figs\\' + str(i3 + 1))
+#后续选择下是开环跑4种功率水平还是只给1、2两个文件绘图
+#for i1 in range(0,4) : # 功率水平（10，20，50，100）4个
+#    if i1 == 0:
+#        fp = 10
+#    elif i1 == 1:
+#        fp = 20
+#    elif i1 == 2:
+#        fp = 50
+#    elif i1 == 3:
+#        fp = 100
+#    for i2 in range(0,3):
+#    #工况（反应性0~-0.2，给水流量+10%，蒸汽阀门1~0.9，上充0.9167，下泄-0.9167，喷淋1.167，电热2.7e5）
+#        data1 = listdir(path,fp,i2 + 1,1)
+#        data2 = listdir(path,fp,i2 + 1,2)
+#        if fp == 100:
+#            if i2 + 1 in [1]:
+#                data1 = data1[range(0,min(int(round((300 * 8.6 / 2))),len(data1[:,0]))),:]
+#                data2 = data2[range(0,min(int(round((300 * 8.6 / 2))),len(data1[:,0]))),:]
+#            elif i2 + 1 in [2,3]:
+#                data1 = data1[range(0,min(int(round((600 * 8.6 / 2))),len(data1[:,0]))),:]
+#                data2 = data2[range(0,min(int(round((600 * 8.6 / 2))),len(data1[:,0]))),:]
+#        elif fp == 50:
+#            if i2 + 1 in [1]:
+#                data1 = data1[range(0,min(int(round((300 * 8.6 / 2))),len(data1[:,0]))),:]
+#                data2 = data2[range(0,min(int(round((300 * 8.6 / 2))),len(data1[:,0]))),:]
+#            elif i2 + 1 in [2]:
+#                data1 = data1[range(0,min(int(round((600 * 8.6 / 2))),len(data1[:,0]))),:]
+#                data2 = data2[range(0,min(int(round((600 * 8.6 / 2))),len(data1[:,0]))),:]
+#        elif fp == 10:
+#            if i2 + 1 in [1]:
+#                data1 = data1[range(0,min(int(round((300 * 8.6 / 2))),len(data1[:,0]))),:]
+#                data2 = data2[range(0,min(int(round((300 * 8.6 / 2))),len(data1[:,0]))),:]
+#            elif i2 + 1 in [3]:
+#                data1 = data1[range(0,min(int(round((600 * 8.6 / 2))),len(data1[:,0]))),:]
+#                data2 = data2[range(0,min(int(round((600 * 8.6 / 2))),len(data1[:,0]))),:]
+#        for i3 in range(0,15): #参数响应图14张
+#            if i3 < 10:
+#                ppp(data1,i3 + 1,path + '\\figs\\' + str(fp) + 'c' + str(i2 + 1) + '_' + str(i3 + 1))
+#            else:
+#                ppp(data2,i3 + 1,path + '\\figs\\' + str(fp) + 'c' + str(i2 + 1) + '_' + str(i3 + 1))
